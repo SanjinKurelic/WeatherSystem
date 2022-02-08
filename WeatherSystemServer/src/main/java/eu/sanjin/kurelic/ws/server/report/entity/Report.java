5ba21e558a3669e6bcb25b00c67440e59a44c132
Sanjin Kurelic <skurelic@croz.net>
2022-02-08 19:44:05 +0100

    Initial ESB support

---
+++ WeatherSystemServer/src/main/java/eu/sanjin/kurelic/ws/server/report/entity/Report.java
@@ -0,0 +1,20 @@
+package eu.sanjin.kurelic.ws.server.report.entity;
+
+import java.math.BigDecimal;
+
+public class Report {
+
+    private Long id;
+
+    private Long stationId;
+
+    private BigDecimal temperature;
+
+    private BigDecimal rainfall;
+
+    private BigDecimal windSpeed;
+
+    private BigDecimal windDirection;
+
+    private BigDecimal barometer;
+}
