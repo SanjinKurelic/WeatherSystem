a2bae49c6232a4537414822a381d8b3147d52fab
Sanjin Kurelic <skurelic@croz.net>
2022-02-08 19:39:47 +0100

    Initial WS server

---
+++ WeatherSystemServer/src/test/java/eu/sanjin/kurelic/ws/server/WeatherSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.sanjin.kurelic.ws.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
